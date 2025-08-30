7676863045718ede7912991e7a7cbdb7acf2e8aa
rahulbhassy <rahulbhassy1122@gmail.com>
2025-08-30 08:41:51 +0530

    Spark Config

--- Enrich_PipelineAll.py
+++ Enrich_PipelineAll.py
@@ -8,12 +8,12 @@ from Enrich_PipelineGRP3 import enrich_grp3_processing_flow
 
 
 @flow(
-    name="Master_Uber_Processing_Pipeline",
+    name="Enrich_Uber_Processing_Pipeline",
     task_runner=DaskTaskRunner(),  # Remove for sequential execution
     description="ETL pipeline for Uber data processing",
     version="1.0"
 )
-def master_processing_flow(load_type: str,runtype: str = 'prod'):
+def enrich_processing_flow(load_type: str,runtype: str = 'prod'):
     """Orchestrates the entire Uber data processing workflow"""
     logger = get_run_logger()
     logger.info(f"Starting master pipeline with load_type: {load_type}")
@@ -31,17 +31,20 @@ def master_processing_flow(load_type: str,runtype: str = 'prod'):
         runtype=runtype,
         wait_for=[enrich_grp1_processing_flow]
     )
+    downstream_dependencies = [enrich_grp1_processing_flow,enrich_grp2_processing_flow]
     logger.info("Starting enrichment flow for Group 3")
     enrich_grp3_processing_flow(
         load_type='full',
         runtype=runtype,
-        initial_load='yes'
+        initial_load='yes',
+        wait_for=downstream_dependencies
+
     )
 
 
 if __name__ == "__main__":
     # Example usage
-    master_processing_flow(
+    enrich_processing_flow(
         load_type="delta",  # or "full" based on your requirement
         runtype='prod'  # or 'dev' based on your environment
     )
